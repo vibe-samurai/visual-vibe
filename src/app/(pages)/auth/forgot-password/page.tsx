a5de32ec5ae149e77ee353a3f5a7e087c343e1cc
Kozzzlova <kozlova.jully@gmail.com>
2025-02-11 11:57:49 +0200

    feat: add forgot password page styles

--- src/app/(pages)/auth/forgot-password/page.tsx
+++ src/app/(pages)/auth/forgot-password/page.tsx
@@ -1,28 +1,43 @@
 'use client'
 
-import { Button, Input, Typography } from '@vibe-samurai/visual-ui-kit'
+import { Button,  Dialog,  Input,  Modal,  Typography } from '@vibe-samurai/visual-ui-kit'
+import { ChangeEvent, useState } from 'react'
+import ReCAPTCHA  from 'react-google-recaptcha'
+
 import s from './page.module.scss'
-import { useState } from 'react'
 
 export default function ForgotPassword() {
   const ERROR_MESSAGE = "User with this email doesn't exist"
   const [errorMessage, setErrorMessage] = useState<string | undefined>(undefined)
+  const [modalMode, setModalMode] = useState<boolean>(false)
+  const [email, setEmail] = useState('')
+  const onChangeEmailHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.currentTarget.value)
+  }
+  const onChangeRecaptcha = (token: any) => {
+    console.log("ReCAPTCHA Token:", token);
+  };
+
   return (
-  <div className={s.forgotPasswordWrapper}>
+  <div className={s.passwordWrapper}>
     <div className={s.cardWrapper}>
-      <Typography className={s.forgotPasswordTitle} as='h1' variant='h1'>Forgot Password</Typography>
-      <Input type='email' errorMessage={errorMessage} label='Email' placeholder='Epam@epam.com'></Input>
-      <Typography className={s.forgotPasswordText} variant='regular-text-14'>Enter your email address and we will send you further instructions </Typography>
+      <Typography  as={"h1"} variant={"h1"}>Forgot Password</Typography>
+      <Input onChange={onChangeEmailHandler} type={"email"} errorMessage={errorMessage} label={"Email"} placeholder={"Epam@epam.com"}></Input>
+      <Typography className={s.passwordText} variant={"regular-text-14"}>Enter your email address and we will send you further instructions </Typography>
       <div className={s.buttonsColumn}>
-        <Button>
-          <Typography variant='h3'>Send Link</Typography>
+        <Button onClick={()=>setModalMode(true)}>
+          <Typography variant={"h3"}>Send Link</Typography>
         </Button>
-        <Button as='a' variant='outlined'>
-          <Typography variant='h3'>Back to Sign In</Typography>
+        <Button as={"a"} href={""} variant={"outlined"}>
+          <Typography variant={"h3"}>Back to Sign In</Typography>
           </Button>
-          <div>Recaptcha</div>
+          <ReCAPTCHA theme={"dark"}   onChange={onChangeRecaptcha} sitekey={"6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"}/>
       </div>
       </div>
-      </div>
+
+      <Dialog className={s.passwordDialog} title={"Email sent"} size={"sm"} open={modalMode} onConfirmButtonClick={() => { setModalMode(false) }} onClose={ () => { setModalMode(false) }} confirmButtonText={ 'OK'}>We have sent a link to confirm your email to {email}</Dialog>
+     
+    </div>
+  
   )
 }
