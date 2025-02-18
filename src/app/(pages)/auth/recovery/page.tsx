40bac41bbde2ca623c3d682c6e55333dc80463c3
Kozzzlova <kozlova.jully@gmail.com>
2025-02-19 01:36:11 +0200

    refactoring: create new password fprm moved to ui

--- src/app/(pages)/auth/recovery/page.tsx
+++ src/app/(pages)/auth/recovery/page.tsx
@@ -1,49 +1,27 @@
 'use client'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { FetchBaseQueryError } from '@reduxjs/toolkit/query'
-import { Button, Card, Typography, Loader } from '@vibe-samurai/visual-ui-kit'
+
+import { Card, Typography, Loader } from '@vibe-samurai/visual-ui-kit'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { useEffect, useState } from 'react'
-import { useForm } from 'react-hook-form'
-import { useDispatch, useSelector } from 'react-redux'
-import { z } from 'zod'
 
-import {
-  useCheckRecoveryCodeMutation,
-  useCreateNewPasswordMutation,
-  useTerminateAllSessionsMutation,
-} from '@/app/services/vibeVisualApi'
-import { AppStore } from '@/app/store/store'
-import { errorMessages, password } from '@/features/auth/model/validation/validationScheme'
-import { FormInput } from '@/shared/components/form-input/form-input'
+import { useCheckRecoveryCodeMutation } from '@/app/services/vibeVisualApi'
+import { useAppDispatch, useAppSelector } from '@/app/store/store'
+import { recoverySelector } from '@/features/auth/model/selectors/recoverySelector'
+import CreateNewPasswordForm from '@/features/auth/ui/create-new-password/CreateNewPasswordForm'
 import { PATH } from '@/shared/constants/PATH'
 
 import s from './page.module.scss'
-import { setRecoveryCode } from './recoverySlice'
-
-const newPasswordSchema = z
-  .object({
-    password: password(errorMessages.password),
-    passwordConfirmation: z.string(),
-  })
-  .refine(data => data.password === data.passwordConfirmation, {
-    message: 'The passwords must match',
-    path: ['passwordConfirmation'],
-  })
-
-type newPasswordValues = z.infer<typeof newPasswordSchema>
+import { setRecoveryCode } from '../../../../features/auth/model/slices/recoverySlice'
 
 export default function RecoveryPassword() {
   const router = useRouter()
-  const dispatch = useDispatch()
-  const recoveryCode = useSelector((state: AppStore) => state.recovery.recoveryCode)
+  const dispatch = useAppDispatch()
+  const recovery = useAppSelector(recoverySelector)
+  const recoveryCode = recovery.recoveryCode
 
   const [checkRecoverCode, { isLoading }] = useCheckRecoveryCodeMutation()
-  const [createNewPassword] = useCreateNewPasswordMutation()
-  const [terminateAllSessions] = useTerminateAllSessionsMutation()
 
   const [isLoadingPage, setIsLoadingPage] = useState(true)
-  const [serverError, setServerError] = useState<undefined | string>(undefined)
   const searchParams = useSearchParams()
 
   useEffect(() => {
@@ -58,35 +36,10 @@ export default function RecoveryPassword() {
           router.replace(PATH.AUTH.RECOVERY)
         })
         .catch(() => {
-          router.push('/auth/resend-link')
+          router.push(PATH.AUTH.RECOVERY_LINK_EXPIRED)
         })
     }
   }, [dispatch, checkRecoverCode, router, searchParams])
-
-  const { control, handleSubmit, reset, trigger, getValues } = useForm<newPasswordValues>({
-    defaultValues: { password: '', passwordConfirmation: '' },
-    mode: 'onSubmit',
-    resolver: zodResolver(newPasswordSchema),
-  })
-
-  const onSubmit = async () => {
-    const isValid = await trigger('passwordConfirmation')
-
-    if (isValid) {
-      const newPassword = getValues('passwordConfirmation')
-
-      try {
-        await createNewPassword({ newPassword, recoveryCode }).unwrap()
-        reset()
-        await terminateAllSessions()
-        router.push(PATH.AUTH.LOGIN)
-      } catch (error) {
-        const err = error as FetchBaseQueryError & { data?: { messages?: { message: string }[] } }
-
-        setServerError(err.data?.messages?.[0]?.message)
-      }
-    }
-  }
 
   if (isLoading || isLoadingPage) return <Loader />
 
@@ -95,38 +48,7 @@ export default function RecoveryPassword() {
       <Typography as={'h1'} className={s.titleText} variant={'h1'}>
         Create New Password
       </Typography>
-
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className={s.buttonsColumn}>
-          {' '}
-          <FormInput
-            name={'password'}
-            control={control}
-            label={'New password'}
-            type={'password'}
-            placeholder={'******************'}
-          />
-          <FormInput
-            name={'passwordConfirmation'}
-            control={control}
-            label={'Password confirmation'}
-            type={'password'}
-            placeholder={'******************'}
-          />
-        </div>
-        {serverError && (
-          <Typography variant={'regular-text-14'} className={s.errorText}>
-            {serverError}
-          </Typography>
-        )}
-        <Typography className={s.passwordText} variant={'regular-text-14'}>
-          Your password must be between 6 and 20 characters
-        </Typography>
-
-        <Button type={'submit'} disabled={false} fullWidth>
-          <Typography variant={'h3'}>Create new password</Typography>
-        </Button>
-      </form>
+      <CreateNewPasswordForm />
     </Card>
   )
 }
