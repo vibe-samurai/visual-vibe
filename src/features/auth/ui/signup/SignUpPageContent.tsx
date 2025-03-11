b1aa32a7bc1e2910852d81de20a282988e0de5e1
Julia <veluat@gmail.com>
2025-03-11 11:22:28 +0300

    refactor: update project structure

--- src/features/auth/ui/signup/SignUpPageContent.tsx
+++ src/features/auth/ui/signup/SignUpPageContent.tsx
@@ -7,9 +7,9 @@ import { useState, useCallback } from 'react'
 import { useForm, SubmitHandler } from 'react-hook-form'
 
 import { useAppDispatch, useAppSelector } from '@/app/store/store'
-import { SignUpForm } from '@/entities'
 import { useSignupMutation } from '@/features/auth/api'
 import { OAuthBlock } from '@/shared'
+import { SignUpForm } from '@/shared/components/signup-form'
 import { PATH } from '@/shared/constants/PATH'
 import { useRequestError } from '@/shared/hooks/useRequestError'
 
